9b721c748a07a8880f246e94c13adb5405abf4c2
chigivera <ayman.benchamkha@gmail.com>
2025-02-16 18:56:54 +0000

    landing page

--- src/components/stats-section.tsx
+++ src/components/stats-section.tsx
@@ -2,16 +2,16 @@
 
 export default function StatsSection() {
     return (
-        <section className="relative z-40 w-full p-10 text-center bg-transparent">
+        <section className="relative z-40 flex items-center w-full p-10 text-center bg-transparent lg:-mt-10">
         {/* Fluid background */}
-        <div className="absolute left-0 right-0 hidden lg:block -top-36">
+        <div className="absolute left-0 right-0 hidden lg:block ">
             <svg className="w-full h-30" viewBox="0 0 999 294" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <path d="M999 0C801.424 95.1031 390.335 124.508 0 101.279V217.996C380.918 290.389 599.831 326.576 999 255.667V0Z" fill="#1D4ED8"/>
+            <path d="M999 0C801.424 95.1031 390.335 95.2289 0 72V182C380.918 254.393 599.831 265.576 999 194.667V0Z" fill="#1D4ED8"/>
             </svg>
         </div>
     
         {/* Content */}
-        <div className="relative pt-4 mx-auto max-w-7xl px-auto">
+        <div className="relative mx-auto max-w-7xl px-auto">
             <div className="grid w-full grid-cols-1 gap-12 md:grid-cols-3">
                 <div className="space-y-2">
                     <div className="text-6xl font-bold text-blue-700 lg:text-white sm:text-5xl">90%</div>
